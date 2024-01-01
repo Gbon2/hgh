6a2f5aee43a740ee654c9ade1938482896c2b3b1
Georgij <yak@yandex.ru>
2024-01-01 15:57:25 +0300

    Добавили нового кода

--- gg1.py
+++ gg1.py
@@ -1,1 +1,12 @@
 print("hello world!")
+while True:
+    n = int(input("Введите число: "))
+    if n > 10:
+        print("Это число больше 10 ")
+    if n == 10:
+        print("10 - красивое число")    
+    else:
+        print("Это число меньше 10")  
+
+
+      
